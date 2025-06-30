82385c8cef033e29e0dcfbfc41b3cfe69d80f201
arsalan-smit <arsalangg05@gmail.com>
2025-07-01 03:42:46 +0500

    create new route   /admin/allblogs , /admin/allUser  , /admin/userUpdate/:userId, /likeblogs/:id

--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -1,10 +1,14 @@
 import express from 'express';
 const router = express.Router();
-import { login, signup, createblogs, Updateblogs, deleteblogs, blogs, getBlogByUserId, getBlogById } from '../Controller/controller.js';
+import {createblogs, Updateblogs, deleteblogs, blogs, getBlogByUserId, getBlogById, likeblogs , allblogs, allUser, userUpdate } from '../Controller/controller.js';
 
-router.post('/login',login);
+// router.post('/login',login);
 
-router.post('/signup', signup);
+// router.post('/signup', signup);
+
+router.get('/admin/allblogs', allblogs);
+router.get('/admin/allUser', allUser);
+router.put('/admin/userUpdate/:userId', userUpdate);
 
 router.post('/createblogs', createblogs)
 
@@ -12,10 +16,12 @@ router.patch('/updateblogs/:id', Updateblogs)
 
 router.delete('/deleteblogs/:id', deleteblogs)
 
-router.get('/blogs', blogs);
+// router.get('/blogs', blogs);
 
 router.get('/blogs/:id', getBlogByUserId);
 
 router.get('/blog/:id', getBlogById);
+
+router.patch('/likeblogs/:id', likeblogs);
 
 export default router;
