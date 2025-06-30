e625b1d4db579a36b5f02792c3b2062c3ebda9e8
arsalan-smit <arsalangg05@gmail.com>
2025-07-01 03:47:46 +0500

    add features after login navbar change

--- Frontend/Blog-App/src/component/Navbar.jsx
+++ Frontend/Blog-App/src/component/Navbar.jsx
@@ -1,29 +1,70 @@
 import React from "react";
-import { AppBar, Toolbar, Button, Typography, Box } from "@mui/material";
-import { Link } from 'react-router-dom';
-
+import { AppBar, Toolbar, Button, Typography, Box, TextField } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ authCheck }) => {
+  // const [inputValue, setinput] = useState("");
+  // const [searchTerm, setSearchTerm] = useState("");
+  // const handleSearchChange = (e) => {
+  //   setSearchTerm(e.target.value);
+  // };
+
   return (
     <AppBar position="sticky">
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', backgroundColor:'black', padding:'25px'}}>
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          backgroundColor: "black",
+          padding: "25px",
+        }}
+      >
         {/* Left Side - MYBLOG */}
         <Typography variant="h5" fontWeight={700}>
           MY BLOG
         </Typography>
+
+        {/* {authCheck && (
+          <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <TextField
+              value={searchTerm}
+              onChange={handleSearchChange}
+              label="Search"
+              variant="outlined"
+              size="small"
+              sx={{ marginRight: 2, backgroundColor: "white", borderRadius: "5px" }}
+            />
+          </Box>
+        )} */}
 
         {/* Right Side - Conditional Buttons */}
         <Box>
           {authCheck ? (
             // If authCheck is true, show Home, MyBlog, and Create Blog
             <>
-              <Button color="inherit" component={Link} to="/">Home</Button>
-              <Button color="inherit" component={Link} to="/myblog">MyBlog</Button>
-              <Button color="inherit" component={Link} to="/createblog">Create Blog</Button>
+              {/* <TextField
+              value={searchTerm}
+              onChange={handleSearchChange}
+              label="Search"
+              variant="outlined"
+              size="small"
+              sx={{ marginRight: 2, backgroundColor: "white", borderRadius: "5px" }}
+            /> */}
+              <Button color="inherit" component={Link} to="/">
+                Home
+              </Button>
+              <Button color="inherit" component={Link} to="/myblog">
+                MyBlog
+              </Button>
+              <Button color="inherit" component={Link} to="/createblog">
+                Create Blog
+              </Button>
             </>
           ) : (
             // If authCheck is false, show Login button
-            <Button color="inherit" component={Link} to="/login">Login</Button>
+            <Button color="inherit" component={Link} to="/login">
+              Login
+            </Button>
           )}
         </Box>
       </Toolbar>
@@ -32,4 +73,3 @@ const Navbar = ({ authCheck }) => {
 };
 
 export default Navbar;
-
